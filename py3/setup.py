56390e06729542d5d03b77dc203818595fe3a85c
Mohsen Banan <form@mohsen.banan.1.byname.net>
2025-01-28 13:42:31 -0800

    Updated setup.py.

--- py3/setup.py
+++ py3/setup.py
@@ -43,7 +43,7 @@ def longDescription():
 # b:py3:pypi:setup/version Arguments  :forSys t :forPyPi t :constant "666"
 ####+BEGIN: b:py3:pypi:setup/version :comment "Auto Detected"
 
-# ./pypiUploadVer DID NOT exist -- forPypiVersion=1.00 -- forSysVersion=0.92 -- constant=NA
+# ./pypiUploadVer DID NOT exist -- forPypiVersion=1.01 -- forSysVersion=0.92 -- constant=NA
 def pkgVersion():
         return '0.92'
 
@@ -52,7 +52,7 @@ def pkgVersion():
 # b:py3:pypi:setup/requires :extras ; :requirements "requirements.txt" (bring here requirements.txt)
 ####+BEGIN: b:py3:pypi:setup/requires :extras ()
 
-requires = [ 
+requires = [
 "blee",
 "blee.csPlayer",
 "blee.icmPlayer",
@@ -70,7 +70,7 @@ requires = [
 # b:py3:pypi:setup/scripts :comment
 ####+BEGIN: b:py3:pypi:setup/scripts :comment ""
 
-scripts = [ 
+scripts = [
 'bin/facter-assemble.cs',
 'bin/facter-binsPrep.cs',
 'bin/facter-cbs-is-p-sysd.cs',
@@ -85,7 +85,7 @@ scripts = [
 # b:py3:pypi:setup/dataFiles :comment
 ####+BEGIN: b:py3:pypi:setup/dataFiles :comment "Instead of ./MANIFEST.in or in pyproject.toml"
 
-data_files = [ 
+data_files = [
 (' ',  ['lh-agpl3-LICENSE.txt', '_description.org', 'README.rst']),
 ]
 ####+END:
