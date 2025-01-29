296fbb91228d21c6c47b93d8207c2cb750c2bf6c
Mohsen Banan <form@mohsen.banan.1.byname.net>
2025-01-29 12:26:18 -0800

    Switched to generic which.

--- py3/bin/facter-cbs-is-p-sysd.cs
+++ py3/bin/facter-cbs-is-p-sysd.cs
@@ -3,7 +3,7 @@
 ####+BEGIN: b:prog:file/particulars :authors ("./inserts/authors-mb.org")
 """ #+begin_org
 * *[[elisp:(org-cycle)][| Particulars |]]* :: Authors, version
-** This File: /bisos/git/bxRepos/bisos-pip/graphviz-cs/py3/bin/exmpl-graphviz.cs
+** This File: /bisos/git/bxRepos/bisos-pip/facter/py3/bin/facter-cbs-is-p-sysd.cs
 ** Authors: Mohsen BANAN, http://mohsen.banan.1.byname.net/contact
 #+end_org """
 ####+END:
@@ -39,15 +39,15 @@ cba_sysd_seed.setup(
 )
 
 
-####+BEGIN: b:py3:cs:seed/withWhich :seedName "/bisos/git/bxRepos/bisos-pip/capability/py3/bin/cba-sysd.cs"
+####+BEGIN: b:py3:cs:seed/withWhich :seedName "cba-sysd.cs"
 """ #+begin_org
-*  _[[elisp:(blee:menu-sel:outline:popupMenu)][±]]_ _[[elisp:(blee:menu-sel:navigation:popupMenu)][Ξ]]_ [[elisp:(outline-show-branches+toggle)][|=]] [[elisp:(bx:orgm:indirectBufOther)][|>]] *[[elisp:(blee:ppmm:org-mode-toggle)][|N]]*  seed       [[elisp:(outline-show-subtree+toggle)][||]] <</bisos/git/bxRepos/bisos-pip/capability/py3/bin/cba-sysd.cs>>   [[elisp:(org-cycle)][| ]]
+*  _[[elisp:(blee:menu-sel:outline:popupMenu)][±]]_ _[[elisp:(blee:menu-sel:navigation:popupMenu)][Ξ]]_ [[elisp:(outline-show-branches+toggle)][|=]] [[elisp:(bx:orgm:indirectBufOther)][|>]] *[[elisp:(blee:ppmm:org-mode-toggle)][|N]]*  seed       [[elisp:(outline-show-subtree+toggle)][||]] <<cba-sysd.cs>>   [[elisp:(org-cycle)][| ]]
 #+end_org """
 import shutil
 import os
 import sys
 
-seedName = '/bisos/git/bxRepos/bisos-pip/capability/py3/bin/cba-sysd.cs'
+seedName = 'cba-sysd.cs'
 seedPath = shutil.which(seedName)
 if seedPath is None:
     print(f'sys.exit() --- which found nothing for {seedName} --- Aborting')
