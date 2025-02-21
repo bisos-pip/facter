1d29c40282bce74049cb83550108778fd615ef90
Mohsen Banan <form@mohsen.banan.1.byname.net>
2025-02-20 22:58:12 -0800

    0.17 -roInv to roInv- conversion.

--- py3/setup.py
+++ py3/setup.py
@@ -43,7 +43,7 @@ def longDescription():
 # b:py3:pypi:setup/version Arguments  :forSys t :forPyPi t :constant "666"
 ####+BEGIN: b:py3:pypi:setup/version :comment "Auto Detected"
 
-# ./pypiUploadVer DID NOT exist -- forPypiVersion=1.16 -- forLocalVersion=0.92 -- constant=NA
+# ./pypiUploadVer DID NOT exist -- forPypiVersion=1.17 -- forLocalVersion=0.92 -- constant=NA
 def pkgVersion():
         return '0.92'
 
@@ -74,9 +74,9 @@ scripts = [
 'bin/facter-cbs-is-p-sysd.cs',
 'bin/facter.cs',
 'bin/facter-perfSysd-obsolete.cs',
+'bin/facter-roInv.cs',
+'bin/facter-roPerf.cs',
 'bin/facter-roPerf-sysd.cs',
-'bin/roInv-facter.cs',
-'bin/roPerf-facter.cs',
 ]
 ####+END:
 
