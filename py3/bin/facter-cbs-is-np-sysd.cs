0c3340b1d8166c98dcc0c082661992674d931b42
Mohsen Banan <form@mohsen.banan.1.byname.net>
2025-03-18 12:49:55 -0700

    1.24 renamed to facter-cbs-is-np-sysd.cs

--- py3/bin/facter-cbs-is-np-sysd.cs
+++ py3/bin/facter-cbs-is-np-sysd.cs
@@ -3,17 +3,19 @@
 ####+BEGIN: b:prog:file/particulars :authors ("./inserts/authors-mb.org")
 """ #+begin_org
 * *[[elisp:(org-cycle)][| Particulars |]]* :: Authors, version
-** This File: /bisos/git/bxRepos/bisos-pip/facter/py3/bin/facter-cbs-is-p-sysd.cs
+** This File: /l/bin/facter/facter-cbs-is-np-sysd.cs
+** File True Name: /bisos/git/auth/bxRepos/bisos-pip/facter/py3/bin/facter-cbs-is-np-sysd.cs
 ** Authors: Mohsen BANAN, http://mohsen.banan.1.byname.net/contact
 #+end_org """
 ####+END:
 
 """ #+begin_org
-* Panel::  [[file:/bisos/panels/bisos-apps/lcnt/lcntScreencasting/subTitles/_nodeBase_/fullUsagePanel-en.org]]
+* Panel::  [[file:/bisos/panels/bisos-apps/NOTYET/_nodeBase_/fullUsagePanel-en.org]]
 * Overview and Relevant Pointers
-facter-cbs-is-p-sysd.cs
+facter-cbs-is-np-sysd.cs
 cbs: Capability Bundle Specification  -- Based on a cba-sysd.cs seed
 is: An Independent Service  --- /Service Component/
+n:  No Data    -- Materialization is purely based on Realm-BPO
 p:  Platform    -- Materialization is based on Platform-BPO (Site-BPO is not used)
 #+end_org """
 
--- py3/setup.py
+++ py3/setup.py
@@ -43,7 +43,7 @@ def longDescription():
 # b:py3:pypi:setup/version Arguments  :forSys t :forPyPi t :constant "666"
 ####+BEGIN: b:py3:pypi:setup/version :comment "Auto Detected"
 
-# ./pypiUploadVer DID NOT exist -- forPypiVersion=1.23 -- forLocalVersion=0.92 -- constant=NA
+# ./pypiUploadVer DID NOT exist -- forPypiVersion=1.24 -- forLocalVersion=0.92 -- constant=NA
 def pkgVersion():
         return '0.92'
 
@@ -70,7 +70,7 @@ requires = [
 
 scripts = [
 'bin/facter-assemble.cs',
-'bin/facter-cbs-is-p-sysd.cs',
+'bin/facter-cbs-is-np-sysd.cs',
 'bin/facter.cs',
 'bin/facter-roInv.cs',
 'bin/facter-roPerf.cs',
